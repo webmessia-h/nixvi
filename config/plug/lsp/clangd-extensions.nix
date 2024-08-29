599ef10132ca0a9b02f61b3d5d3c43a848c5a59b
webmessia-h <danylo.volchenko@gmail.com>
2024-08-29 21:11:14 +0300

    trouble.nvim

--- config/plug/lsp/clangd-extensions.nix
+++ config/plug/lsp/clangd-extensions.nix
@@ -1,10 +1,27 @@
 {
   plugins = {
     clangd-extensions = {
-     enable = true;
-     enableOffsetEncodingWorkaround = true;
+      enable = true;
+      enableOffsetEncodingWorkaround = true;
+      ast = {
+        roleIcons = {
+          type = "";
+          declaration = "";
+          expression = "";
+          specifier = "";
+          statement = "";
+          templateArgument = "";
+        };
+        kindIcons = {
+          compound = "";
+          recovery = "";
+          translationUnit = "";
+          packExpansion = "";
+          templateTypeParm = "";
+          templateTemplateParm = "";
+          templateParamObject = "";
+        };
+      };
     };
   };
 }
-
-
