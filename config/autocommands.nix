159cd2b37ae3129ff9a00060acc9c0aa1c1647d4
webmessia-h <danylo.volchenko@gmail.com>
2024-09-09 15:10:14 +0300

    finally a spellcheck

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -21,7 +21,7 @@
         "latex"
         "markdown"
       ];
-      command = "setlocal spell spelllang=en,uk_UA | setlocal conceallevel=2";
+      command = "setlocal spell spelllang=en,uk | setlocal conceallevel=1";
     }
   ];
 }
