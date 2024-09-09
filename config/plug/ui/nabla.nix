4700b875a721528141a7dd79b1a5541e42b6de1d
webmessia-h <danylo.volchenko@gmail.com>
2024-09-09 15:32:44 +0300

    add math equations support

---
+++ config/plug/ui/nabla.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  extraPlugins = with pkgs.vimUtils; [
+    (buildVimPlugin {
+      pname = "nabla.nvim";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "jbyuki";
+        repo = "nabla.nvim";
+        rev = "27a6ea9ed7452bb9e0b19eb0942b5bcf7d051b2f";
+        hash = "";
+      };
+    })
+  ];
+
+  extraConfigLua = ''
+    nnoremap <leader>p :lua require("nabla").popup()<CR> " Customize with popup({border = ...})  : `single` (default), `double`, `rounded`
+  '';
+}
