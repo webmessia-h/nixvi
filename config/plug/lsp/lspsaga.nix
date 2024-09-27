fb900fe2d35c7bfa95a3278f3a605860474f2881
webmessia-h <danylo.volchenko@gmail.com>
2024-09-27 18:35:10 +0300

    fix lspsaga and clang

--- config/plug/lsp/lspsaga.nix
+++ config/plug/lsp/lspsaga.nix
@@ -6,7 +6,7 @@
     };
     ui = {
       border = "rounded"; # One of none, single, double, rounded, solid, shadow
-      codeAction = "⁕"; # Can be any symbol you want 💡
+      codeAction = "⁕ "; # Can be any symbol you want 💡
     };
     hover = {
       openCmd = "!firefox"; # Choose your browser
